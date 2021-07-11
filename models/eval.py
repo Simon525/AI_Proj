781faa2a494e32fcff2dfb65d4e805711ad2db9c
zhenhao1886 <ng.zhenhao@dhs.sg>
2021-07-11 07:26:57 +0000

    added: cyclegan

--- models/eval.py
+++ models/eval.py
@@ -1,7 +1,10 @@
 #to extract features from inception, we need to rebuild the model.
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 from torchvision.models import inception_v3
+import numpy as np
+import scipy
 
 class InceptionV3(nn.Module):
     """Pretrained InceptionV3 network returning feature maps"""
@@ -137,6 +140,7 @@ class InceptionV3(nn.Module):
 
         return outp
     
+
 #define FID
 #A and B are two 2048 size vectors
 def calculate_FID_distance(A, B):
@@ -151,17 +155,18 @@ def calculate_FID_distance(A, B):
     
     return ssq + np.trace(cov_A+cov_B-2*covmean)
 
-def calculate_d_ij(real_vec, gen_vec):
+def calculate_dij(real_vec, gen_vec):
     return 1 - np.dot(real_vec, gen_vec)/(np.linalg.norm(real_vec)*np.linalg.norm(gen_vec))
 
 def calculate_memorization(gen_matrix, real_matrix, eps):
     #dimension of real: M*2048, gen: N*2048
     #final score matrix: N*M (gen by real)
-    score = np.zeros(gen_matrix.shape[0], real_matrix.shape[0])
+    print(gen_matrix)
+    score = np.zeros((gen_matrix.shape[0], real_matrix.shape[0]))
     #e.g 3 by 2048
     for i in range(gen_matrix.shape[0]):
         for j in range(real_matrix.shape[0]):
-            score[i,j] = calculate_dij(real_matrix[j,:], gen_matrix[i:])
+            score[i,j] = calculate_dij(real_matrix[j,:], gen_matrix[i,:])
     
     #average all the generated images
     d = np.mean(np.min(score, axis=0))
@@ -172,6 +177,16 @@ def calculate_memorization(gen_matrix, real_matrix, eps):
     
     return dthr
     
-def get_mifid(gen_matrix, real_matrix, eps = 10e-15):
-    d = calculate_FID_distance(gen_matrix)
-    calculate_memorization(gen_matrix, real_matrix, eps)
+def get_mifid(gen_matrix, real_matrix, eps = 1e-15):
+    d = calculate_FID_distance(gen_matrix, real_matrix)
+    mem = calculate_memorization(gen_matrix, real_matrix, eps)
+    return d/mem
+
+
+#run this to get mifid score
+def get_mifid_from_images(gen_img, real_img, eps = 1e-15):
+    v3 = torch.load('inceptionv3.pkl')
+    gen_matrix = v3(gen_img).squeeze().numpy()
+    real_img = v3(real_img).squeeze().numpy()
+    return get_mifid(gen_matrix, real_matrix)
+    
